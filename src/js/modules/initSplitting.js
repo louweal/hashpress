67d0338fdc24644194c04463f110835a71233bbe
louweal <anneloeslouwe@live.nl>
2024-11-11 21:28:57 +0100

    improved some styling

--- src/js/modules/initSplitting.js
+++ src/js/modules/initSplitting.js
@@ -2,7 +2,7 @@ import 'splitting/dist/splitting.css';
 import Splitting from 'splitting';
 
 export const initSplitting = () => {
-    let h1 = document.querySelector('.hero .editor h1');
+    let h1 = document.querySelector('h1'); // first h1 on the page
     if (!h1) return;
 
     h1.setAttribute('data-splitting', 'chars');
