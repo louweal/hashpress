365c0071a98ed8fde53c90beb1ea5f5e591009ba
louweal <anneloeslouwe@live.nl>
2024-11-12 14:45:26 +0100

    improve 404 layout

--- template-parts/hero/hero-404.php
+++ template-parts/hero/hero-404.php
@@ -8,9 +8,15 @@
 ?>
 
 <header class="hero">
-    <div class="container">
-        <div class="editor">
-            <h1>Page not found</h1>
+    <div class="container h-full">
+        <div class="hero__inner">
+            <div class="grid lg:grid-cols-12 w-full gap-5">
+                <div class="lg:col-start-3 col-span-6 flex flex-col items-start self-center">
+                    <div class="editor">
+                        <h1>Page not found</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </header>
